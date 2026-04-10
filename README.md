53de9a7d71029b16cdc4ad56fb834b115707b90a
Jonah Morgan <jlamorgan5@gmail.com>
2026-04-10 02:14:27 +0000

    Add more details

---
+++ README.md
@@ -0,0 +1,3 @@
+# Personal Artix Installation
+
+All of my custom configuration files are stored here in case something bad happens to my install.
--- root/install.sh
+++ root/install.sh
@@ -1,3 +1,13 @@
+printf "label: gpt\n,256M,U\n,,\n" | sfdisk --wipe always --wipe-partitions always /dev/nvme0n1
+mkfs.fat -F 32 -n "ESP" /dev/nvme0n1p1
+mkfs.f2fs -l "ROOT" /dev/nvme0n1p2
+mount /dev/nvme0n1p2 /mnt
+mkdir -p /mnt/boot/efi
+mount /dev/nvme0n1p1 /mnt/boot/efi
+fallocate -l 8G /mnt/swapfile
+chmod 600 /mnt/swapfile
+mkswap /mnt/swapfile
+swapon /mnt/swapfile
 dinitctl start ntpd
 basestrap /mnt base base-devel dinit elogind-dinit
 basestrap /mnt linux-ck linux-firmware linux-headers mkinitcpio
@@ -7,8 +17,9 @@ locale-gen
 echo "root:morgan" | chpasswd
 useradd -m jonah
 echo "jonah:morgan" | chpasswd
-pacman -S intel-ucode networkmanager-dinit dhcpd msedit egummiboot wpa_supplicant
+pacman -S amd-ucode networkmanager-dinit dhcpd msedit egummiboot wpa_supplicant
 ln -s ../NetworkManager /etc/dinit.d/boot.d/
+mkinitcpio -p linux-ck
 exit
 unmout -lR /mnt
 reboot
